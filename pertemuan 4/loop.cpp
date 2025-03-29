613f96c0c4caff9e400c8231377c5f9a64d18450
FaridSaniAhdaputra <faridsani.ahdaputra@gmail.com>
2025-03-29 17:11:18 +0700

    pertemuan 4 : perulangan

---
+++ pertemuan 4/loop.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // goto label 
+    // hello world, fasilkom-Ti, Ilmu Komputer, IKLC
+    // -> Hello World, IKLC, ilmu komputer, fasilkom-Ti 
+
+//     a: 
+//         cout << "Hello World" << endl;
+//         goto d;
+        
+//     b: 
+//         cout << "Fasilkom-Ti" << endl;
+//         return 0;
+//     c:
+//         cout << "Ilmu Komputer" << endl;
+//         goto b;
+//     d:
+//         cout << "IKLC" << endl;
+//         goto c;
+
+    // menampilkan bilangan genap dari 10-> 2 dengan goto
+    // int i = 10;
+    // genap:
+    // if (i % 2 == 0 ){
+    //     cout << i << " " ;
+    //  } i--;
+    // if (i >= 2){
+    //     goto genap ;
+    // }
+
+    //statment while == berjalan terus ketika keadaannya benar
+    // int i = 1 ;
+    // while (i <= 10){
+    //     if ( i % 2 == 0 ){
+    //         cout << i << " ";
+    //     } i++;
+    // }
+
+    //statment do while == berjalan sekali walaupun keadaannya salah
+    // int i = 1;
+    // do{
+    //     cout << i << endl;
+    // } while (i <= 0); // 1 <= 0 == false
+
+    //statment for == untuk perulangan yang sudah ditentukan
+    // for (inisialisasi, kondisi, increase)
+    // for (int i = 1; i <=10 ; i+=2){
+    //     cout << " Hello world " << endl; // i = 1, 3, 5, 7, 9 == maka akan munduk sebanyak 5 kali 
+    // }
+
+    // Nested for 
+    // ***** 5 x 5 
+    // for (int i = 1; i <= 5; i++){
+    //     for (int j = 1; j <= 5; j++){
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //segitiga siku-siku
+//     for (int i = 1; i<= 5; i++){
+//         for (int j = 1; j<= i; j++){
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+// }
